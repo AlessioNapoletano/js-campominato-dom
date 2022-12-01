9555bf215f0c252cd31bed421cc27ce3bbd6cd05
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-12-01 17:32:33 +0100

    Create List bomb

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 const mainElement = document.querySelector("main");
-const bomb = [10, 30, 45, 50, 60, 70];
+
+
+
+//RIEMPIO l'array delle bombe
+
+
+
 let punteggio = 0;
 const button = document.querySelector("button");
 //STAMPO il valore del punteggio
@@ -8,6 +14,13 @@ button.addEventListener("click", function () {
 
     //SVUOTO l'elemento main Element, resetto
     mainElement.innerHTML = "";
+    
+    //ARRAY che contiene le 16 bombe
+    const bombList = [];
+    while (bombList.length < 16) {
+        bombList.push(getRandomBomb(bombList, 1, 100));
+    }
+    console.log(bombList);
 
     //CREO il divParent che conterra le piccole caselle, AGGIUNGO le classi, lo INSERISCO all'interno del mainElement
     const newParentDivElement = document.createElement("div");
@@ -29,13 +42,13 @@ function createSquare(number) {
 
     newElement.addEventListener("click", function () {
         newElement.classList.toggle("bg-danger");
-        if (bomb.includes(number)) {
+        if (bombList.includes(number)) {
             console.log("alla casella: " + number + " c'era na bella bombazza")
             alert("HAI PERSO , hai totalizzato: " + punteggio + " punti");
             punteggio = 0;
             mainElement.innerHTML = "";
             outputPoint.innerHTML = "Il tuo punteggio: " + punteggio;
-            
+
         } else {
             punteggio++;
             outputPoint.innerHTML = "Il tuo punteggio: " + punteggio;
@@ -44,7 +57,20 @@ function createSquare(number) {
     return newElement;
 }
 
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-function getRadomUniqueNumber(blackList, min, max) {
-    let isValid
+function getRandomBomb(bombList, min, max) {
+    let isUnique = false;
+    let randomBombPosition;
+
+    while (isUnique === false) {
+        randomBombPosition = randomNumber(min, max);
+        if (!bombList.includes(randomBombPosition)) {
+            isUnique = true;
+        }
+    }
+
+    return randomBombPosition;
 }
